d5c5805ce169850ff03878e2df9ef8ab75b00f17
Jacob Brunson <github@jacobbrunson.com>
2021-10-06 23:34:30 +0000

    Added contains

--- jacobsjsonschema/draft6.py
+++ jacobsjsonschema/draft6.py
@@ -1,14 +1,15 @@
 
-from typing import Union, Optional
+from typing import Union, Optional, List
 from math import modf
 
-from .draft4 import Validator as Draft4Validator, JsonTypes
+from .draft4 import Validator as Draft4Validator, JsonTypes, JsonSchemaValidationError
 
 class Validator(Draft4Validator):
 
     def __init__(self, schema:dict, lazy_error_reporting:bool=False):
         super().__init__(schema, lazy_error_reporting)
         self.validators["const"] = self._validate_const
+        self.validators["contains"] = self._validate_contains
     
     def _validate_type_integer(self, data:Union[int, float], schema_type) -> bool:
         if isinstance(data, float):
@@ -22,6 +23,27 @@ class Validator(Draft4Validator):
     def _validate_const(self, data:JsonTypes, const_value:dict) -> bool:
         if data != const_value:
             self._report_validation_error("The data value '{}' was not the const value '{}'".format(data, const_value), data, const_value)
+        return True
+
+    def _contains_count(self, data:List[JsonTypes], schema:dict) -> int:
+        occurances = 0
+        for item in data:
+            self._temp_ignore_errors = True
+            try:
+                if self.validate(item, schema):
+                    occurances += 1
+            except JsonSchemaValidationError:
+                pass
+            except Exception:
+                raise
+            finally:
+                self._temp_ignore_errors = False
+        return occurances
+
+    def _validate_contains(self, data:List[JsonTypes], schema:dict) -> bool:
+        occurances = self._contains_count(data, schema)
+        if occurances < 1:
+            return self._report_validation_error("There weren't any occurances in the array", data, schema)
         return True
 
     def validate(self, data:JsonTypes, schema:Optional[dict]=None) -> bool:
