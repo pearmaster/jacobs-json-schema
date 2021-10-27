bb0ab719ec6982c1a6854acd582bf67c608799af
Jacob Brunson <github@jacobbrunson.com>
2021-10-27 23:22:33 +0000

    First step in using pytest

--- tests/context.py
+++ tests/context.py
@@ -5,7 +5,6 @@ import os
 import sys
 
 addpath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-print(f"Adding {addpath} to path")
 sys.path.insert(0, addpath)
 
 import jacobsjsonschema
--- tests/test_suite.py
+++ tests/test_suite.py
@@ -4,21 +4,37 @@ Uses a bunch of JSON Schema test data to validate a bunch of stuff"""
 import pathlib
 import os.path
 import json
-import unittest
+import pytest
 from pprint import pprint
 
 from .context import jacobsjsonschema
 
 from jacobsjsonschema.draft4 import Validator
 
-class Draft4TestSuite(unittest.TestCase):
+testsuite_dir = pathlib.Path(__file__).parent / 'JSON-Schema-Test-Suite'
+        
 
-    def setUp(self):
-        testsuite_dir = pathlib.Path(__file__).parent / 'JSON-Schema-Test-Suite'
-        test_filepath = os.path.join(testsuite_dir, "tests", "draft4", "enum.json")
-        with open(test_filepath, "r") as test_file:
-            self.test_data = json.load(test_file)[0]
-        self.validator = Validator(self.test_data['schema'])
-    
-    def test_one(self):
-        self.assertTrue(self.validator.validate(self.test_data['tests'][0]))
+def pytest_generate_tests(metafunc):
+    test_filepath = os.path.join(testsuite_dir, "tests", "draft4", "enum.json")
+    with open(test_filepath, "r") as test_file:
+        test_cases = json.load(test_file)
+
+    argnames = ('schema', 'data', 'valid')
+    argvalues = []
+    testids = []
+
+    for test_case in test_cases:
+        
+        for test in test_case['tests']:
+            testids.append(f"draft4 -> enum.json -> {test_case['description']} -> {test['description']}")
+            argvalues.append(pytest.param(test_case['schema'], test['data'], test['valid']))
+
+    metafunc.parametrize(argnames, argvalues, ids=testids)
+
+def test_conformance(schema, data, valid):
+    validator = Validator(schema)
+    if valid:
+        assert validator.validate(data) == valid
+    else:
+        with pytest.raises(jacobsjsonschema.draft4.JsonSchemaValidationError):
+            validator.validate(data)
