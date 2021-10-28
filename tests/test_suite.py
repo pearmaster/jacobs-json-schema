58cae983995f7ddadcc8b3de712c4c0020d957f7
Jacob Brunson <github@jacobbrunson.com>
2021-10-28 03:10:11 +0000

    allow multiple test files

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -15,23 +15,24 @@ testsuite_dir = pathlib.Path(__file__).parent / 'JSON-Schema-Test-Suite'
         
 
 def pytest_generate_tests(metafunc):
-    test_filepath = os.path.join(testsuite_dir, "tests", "draft4", "enum.json")
-    with open(test_filepath, "r") as test_file:
-        test_cases = json.load(test_file)
-
     argnames = ('schema', 'data', 'valid')
     argvalues = []
     testids = []
 
-    for test_case in test_cases:
-        
-        for test in test_case['tests']:
-            testids.append(f"draft4 -> enum.json -> {test_case['description']} -> {test['description']}")
-            argvalues.append(pytest.param(test_case['schema'], test['data'], test['valid']))
+    for testfile in ["enum.json", "items.json"]:
+        test_filepath = os.path.join(testsuite_dir, "tests", "draft4", testfile)
+        with open(test_filepath, "r") as test_file:
+            test_cases = json.load(test_file)
+
+        for test_case in test_cases:
+            
+            for test in test_case['tests']:
+                testids.append(f"draft4 -> {testfile} -> {test_case['description']} -> {test['description']}")
+                argvalues.append(pytest.param(test_case['schema'], test['data'], test['valid']))
 
     metafunc.parametrize(argnames, argvalues, ids=testids)
 
-def test_conformance(schema, data, valid):
+def test_draft4(schema, data, valid):
     validator = Validator(schema)
     if valid:
         assert validator.validate(data) == valid
