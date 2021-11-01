07791ed2af415a893f94c5646a606c372812d774
Jacob Brunson <balena@jacobbrunson.com>
2021-11-01 00:41:13 +0000

    Add test suite

--- tests/test_suite_docs.py
+++ tests/test_suite_docs.py
@@ -7,7 +7,7 @@ import pytest
 from pprint import pprint
 
 from jacobsjsondoc.loader import PrepopulatedLoader
-from jacobsjsondoc.document import Document
+from jacobsjsondoc.document import create_document
 
 from .context import jacobsjsonschema
 
@@ -22,25 +22,28 @@ def pytest_generate_tests(metafunc):
     argvalues = []
     testids = []
 
+    """
     testfile_dir = testsuite_dir / "tests" / "draft4"
 
     for testfile in testfile_dir.glob("*.json"):
         if os.path.basename(testfile) in SPECIAL_TESTS:
             continue
         with open(testfile, "r") as test_file:
+            print(f"Starting with {testfile}")
             ppl = PrepopulatedLoader()
             ppl.prepopulate(os.path.basename(testfile), test_file.read())
-            test_cases = Document(uri=os.path.basename(testfile), resolver=None, loader=ppl)
+            test_cases = create_document(uri=os.path.basename(testfile), resolver=None, loader=ppl)
 
         for test_case in test_cases:
             
             for test in test_case['tests']:
-                testids.append(f"draft4 -> {os.path.basename(testfile)}(doc) -> {test_case['description']} -> {test['description']}")
+                testids.append(f"draft4 -> {os.path.basename(testfile)} -> {test_case['description']} -> {test['description']}")
                 argvalues.append(pytest.param(test_case['schema'], test['data'], test['valid']))
+    """
 
     metafunc.parametrize(argnames, argvalues, ids=testids)
 
-def test_draft4(schema, data, valid):
+def test_draft4_doc(schema, data, valid):
     validator = Validator(schema)
     if valid:
         assert validator.validate(data) == valid
