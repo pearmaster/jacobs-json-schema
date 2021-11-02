062421353ed885d6bb35649c57fe6c2c5fbc1aa5
Jacob Brunson <github@jacobbrunson.com>
2021-11-02 19:13:09 +0000

    Simplify test names

--- tests/test_suite_docs.py
+++ tests/test_suite_docs.py
@@ -5,44 +5,46 @@ import pathlib
 import os.path
 import pytest
 import json
+import sys
 from pprint import pprint
 
-from jacobsjsondoc.loader import PrepopulatedLoader
-from jacobsjsondoc.document import create_document
+if sys.version_info.minor >= 6:
+    from jacobsjsondoc.loader import PrepopulatedLoader
+    from jacobsjsondoc.document import create_document
 
 from .context import jacobsjsonschema
 
 from jacobsjsonschema.draft4 import Validator
 
 testsuite_dir = pathlib.Path(__file__).parent / 'JSON-Schema-Test-Suite'
-        
-SPECIAL_TESTS = ["ref.json", "id.json", "definitions.json", "refRemote.json"]
 
 def pytest_generate_tests(metafunc):
     argnames = ('schema', 'data', 'valid')
     argvalues = []
     testids = []
 
-    testfile_dir = testsuite_dir / "tests" / "draft4"
+    if sys.version_info.minor >= 6:
 
-    for testfile in testfile_dir.glob("*.json"):
-        if os.path.basename(testfile) in SPECIAL_TESTS:
-            continue
-        with open(testfile, "r") as test_file:
-            test_cases = json.load(test_file)
+        testfile_dir = testsuite_dir / "tests" / "draft4"
 
-        for test_case in test_cases:
-            ppl = PrepopulatedLoader()
-            ppl.prepopulate("A", json.dumps(test_case["schema"]))
-            doc = create_document("A", resolver=None, loader=ppl, dollar_id_token=Validator.get_dollar_id_token())
-            
-            for test in test_case['tests']:
-                testids.append(f"draft4 -> {os.path.basename(testfile)} -> {test_case['description']} -> {test['description']}")
-                argvalues.append(pytest.param(doc, test['data'], test['valid']))
+        for testfile in testfile_dir.glob("*.json"):
+            with open(testfile, "r") as test_file:
+                test_cases = json.load(test_file)
+
+            for test_case in test_cases:
+                ppl = PrepopulatedLoader()
+                ppl.prepopulate("A", json.dumps(test_case["schema"]))
+                doc = create_document("A", resolver=None, loader=ppl, dollar_id_token=Validator.get_dollar_id_token())
+                
+                for test in test_case['tests']:
+                    testids.append(f"{os.path.splitext(os.path.basename(testfile))[0]} -> {test_case['description']} -> {test['description']}")
+                    argvalues.append(pytest.param(doc, test['data'], test['valid']))
 
     metafunc.parametrize(argnames, argvalues, ids=testids)
 
 def test_draft4_doc(schema, data, valid):
+    if sys.version_info.minor < 6:
+        pytest.skip()
     validator = Validator(schema)
     if valid:
         assert validator.validate(data) == valid
