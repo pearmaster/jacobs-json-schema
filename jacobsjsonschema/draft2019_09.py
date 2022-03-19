11d7301d0505e6c5482ec70ed7eb818532993df9
Jacob Brunson <github@jacobbrunson.com>
2022-03-19 16:47:39 +0000

    WIP unevaluated properties

--- jacobsjsonschema/draft2019_09.py
+++ jacobsjsonschema/draft2019_09.py
@@ -1,5 +1,6 @@
 
-from typing import Optional, List
+from typing import Optional, List, Dict
+import re
 
 from .json_types import JsonTypes
 from .draft7 import Validator as Draft7Validator, InvalidSchemaError as imported_ise, JsonSchemaValidationError as imported_jsve
@@ -7,8 +8,19 @@ from .draft7 import Validator as Draft7Validator, InvalidSchemaError as imported
 InvalidSchemaError = imported_ise
 JsonSchemaValidationError = imported_jsve
 
+class PropName(str):
+    def __new__(cls, *args, **kwargs):
+        obj = str.__new__(cls, *args, **kwargs)
+        obj._evaluated = False
+        return obj
 
-JsonSchemaValidationError = imported_jsve
+    @property
+    def evaluated(self):
+        return self._evaluated
+
+    @evaluated.setter
+    def evaluated(self, value):
+        self._evaluated = value
 
 class Validator(Draft7Validator):
 
@@ -20,8 +32,9 @@ class Validator(Draft7Validator):
         self.object_validators.update({
             "dependentRequired": self._validate_dependency,
             "dependentSchemas": self._validate_dependency,
+            "properties": self._validate_properties,
+            "patternProperties": self._validate_pattern_properties,
         })
-        
         self._warnings = []
     
     def get_warnings(self):
@@ -64,4 +77,59 @@ class Validator(Draft7Validator):
                 min_contains = schema['minContains'] if 'minContains' in schema else 1
                 retval = self._validate_contains(data, schema['contains'], min_contains, max_contains) and retval
         return retval
+
+    def _validate_properties(self, data:dict, schema:Dict[str,dict]) -> bool:
+        if not isinstance(schema, dict):
+            raise InvalidSchemaError("Properties schema must be an object")
+        if not isinstance(data, dict):
+            return self._report_validation_error("Cannot validate properties on a non-object", data, schema)
+        retval = True
+        for k, v in data.items():
+            if k in schema:
+                retval = self.validate(v, schema[k]) and retval
+                k.evaluated = True
+        return retval
+
+    def _validate_pattern_properties(self, data:Dict[str,JsonTypes], schema:Dict[str,dict]) -> bool:
+        if not isinstance(data, dict):
+            return self._report_validation_error("patternProperties will only validate against an object", data, schema)
+        if not isinstance(schema, dict):
+            raise InvalidSchemaError("patternProperties must be an object")
+        retval = True
+        for regex_expression, subschema in schema.items():
+            pattern = re.compile(regex_expression)
+            for k, v in data.items():
+                if pattern.search(k):
+                    retval = retval and self.validate(v, subschema)
+                    k.evaluated = True
+        return retval
+
+    def _object_validate(self, data:dict, schema:dict) -> bool:
+
+        new_data = dict()
+        for propname, propval in data.items():
+            new_data[PropName(propname)] = propval
+        data = new_data
+
+        retval = True
+
+        for k, validator_func in self.object_validators.items():
+            if k in schema:
+                retval = validator_func(data, schema[k]) and retval
     
+        if 'additionalProperties' in schema:
+            for propname in data.keys():
+                propname.evaluated = True
+            if isinstance(data, dict):
+                property_keys = schema['properties'].keys() if 'properties' in schema else None
+                property_patterns = schema['patternProperties'].keys() if 'patternProperties' in schema else None
+                retval = self._validate_additional_properties(data, schema['additionalProperties'], property_keys, property_patterns) and retval
+            else:
+                retval = self._report_validation_error("Use of additionalProperties to validate a non-object", data, schema['additionalProperties'])
+
+        if 'unevaluatedProperties' in schema:
+            for propname, propvalue in data.items():
+                if not propname.evaluated:
+                    retval = retval and self.validate(propvalue, schema['unevaluatedProperties'])
+
+        return retval
